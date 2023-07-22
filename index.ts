f22feeb5bc3b91d6f571a100244e9cebad316049
Demo <100998142+rathod-seema@users.noreply.github.com>
2023-07-22 09:08:46 +0530

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,113 @@
+//import { notesToPlayInOrder } from "./music-to-play";
+
+//function playMusic() {
+    //const notes = notesToPlayInOrder;
+    
+        let play = document.getElementById("start-playing");
+        function playMusic(){
+            let audio = new Audio("./src/assets/D4.mp3");
+            audio.play()
+        }
+        play.addEventListener("click", playMusic);
+
+         let play = document.getElementById("start-playing");
+        function playMusic(){
+            let audio = new Audio("./src/assets/F4.mp3");
+            audio.play()
+        }
+        play.addEventListener("click", playMusic);
+
+        let play = document.getElementById("start-playing");
+        function playMusic(){
+            let audio = new Audio("./src/assets/G4.mp3");
+            audio.play()
+        }
+        play.addEventListener("click", playMusic);
+
+        let play = document.getElementById("start-playing");
+        function playMusic(){
+            let audio = new Audio("./src/assets/C5.mp3");
+            audio.play()
+        }
+        play.addEventListener("click", playMusic);
+
+        
+        let play = document.getElementById("start-playing");
+        function playMusic(){
+            let audio = new Audio("./src/assets/A4.mp3.mp3");
+            audio.play()
+        }
+        play.addEventListener("click", playMusic);
+    
+        let play = document.getElementById("start-playing");
+        function playMusic(){
+            let audio = new Audio("./src/assets/B4 Flat.mp3");
+            audio.play()
+        }
+        play.addEventListener("click", playMusic);
+
+        let play = document.getElementById("start-playing");
+        function playMusic(){
+            let audio = new Audio("./src/assets/B4 Flat.mp3");
+            audio.play()
+        }
+        play.addEventListener("click", playMusic);
+
+        let play = document.getElementById("start-playing");
+        function playMusic(){
+            let audio = new Audio("./src/assets/C5.mp3");
+            audio.play()
+        }
+        play.addEventListener("click", playMusic);
+
+        let play = document.getElementById("start-playing");
+        function playMusic(){
+            let audio = new Audio("./src/assets/D5.mp3");
+            audio.play()
+        }
+        play.addEventListener("click", playMusic);
+
+        let play = document.getElementById("start-playing");
+        function playMusic(){
+            let audio = new Audio("./src/assets/D5 Sharp.mp3");
+            audio.play()
+        }
+        play.addEventListener("click", playMusic);
+
+        let play = document.getElementById("start-playing");
+        function playMusic(){
+            let audio = new Audio("./src/assets/F5.mp3");
+            audio.play()
+        }
+        play.addEventListener("click", playMusic);
+
+        let play = document.getElementById("start-playing");
+        function playMusic(){
+            let audio = new Audio("./src/assets/G5.mp3");
+            audio.play()
+        }
+        play.addEventListener("click", playMusic);
+
+        let play = document.getElementById("start-playing");
+        function playMusic(){
+            let audio = new Audio("./src/assets/A5.mp3");
+            audio.play()
+        }
+        play.addEventListener("click", playMusic);
+
+        let play = document.getElementById("start-playing");
+        function playMusic(){
+            let audio = new Audio("./src/assets/B5 Flat.mp3");
+            audio.play()
+        }
+        play.addEventListener("click", playMusic);
+
+
+
+
+    
+    
+    // TODO Play these notes one after the other at the pitch and rhythm given in each note
+}
+
+//document.getElementById('start-playing')?.addEventListener('click', playMusic);
